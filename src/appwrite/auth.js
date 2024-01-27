6a7e63afa183156af02a46b17417934e3cf41402
nishok19 <58786882+nishok19@users.noreply.github.com>
2024-01-28 00:05:35 +0530

    Created a Vite Project. Added Auth Service with appwrite

---
+++ src/appwrite/auth.js
@@ -0,0 +1,66 @@
+import { Client, Account, ID } from "appwrite";
+import conf from "../conf/conf";
+
+export class AuthService {
+  client = new Client();
+  account;
+  session;
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+    this.account = new Account(this.client);
+  }
+
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      if (userAccount) {
+        return this.login(email, password);
+      } else {
+        throw Error;
+      }
+    } catch (err) {
+      console.log("Creation of account failed in createAccount-auth.js, ", err);
+      throw Error;
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailSession(email, password);
+    } catch (err) {
+      console.log("Login failed in login-auth.js, ", err);
+      throw Error;
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (err) {
+      console.log(
+        "Getting current user failed in getCurrentUser-auth.js, ",
+        err
+      );
+      throw Error;
+    }
+  }
+
+  async logout() {
+    try {
+      return await this.account.deleteSessions();
+    } catch (err) {
+      console.log("Logout user failed in logout-auth.js, ", err);
+      throw Error;
+    }
+  }
+}
+
+export default authService = new AuthService();
