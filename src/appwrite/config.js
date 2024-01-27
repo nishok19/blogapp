910656320d6541304eaa4438a1396c6b30b9bbd1
nishok19 <58786882+nishok19@users.noreply.github.com>
2024-01-28 01:27:51 +0530

    Created Database and Storage services using appwrite

---
+++ src/appwrite/config.js
@@ -0,0 +1,123 @@
+import conf from "../conf/conf";
+import { Client, Databases, Storage, Query, ID } from "appwrite";
+
+export class Service {
+  client = new Client();
+  databases;
+  bucket;
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+    this.databases = new Databases(this.client);
+    this.bucket = new Storage(this.client);
+  }
+
+  async getPost(slug) {
+    try {
+      return await this.databases.getDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug
+      );
+    } catch (err) {
+      console.log("Get post failed in getPost-config.js, ", err);
+      return false;
+    }
+  }
+
+  async getPosts(queries = [Query.equal("status", "active")]) {
+    try {
+      return await this.databases.listDocuments(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        queries
+      );
+    } catch (err) {
+      console.log("Get posts failed in getPosts-config.js, ", err);
+      return false;
+    }
+  }
+
+  async createPost({ title, slug, content, featuredImage, status, userId }) {
+    try {
+      return await this.databases.createDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug,
+        { title, slug, content, featuredImage, status, userId }
+      );
+    } catch (err) {
+      console.log("Create post failed in createPost-config.js, ", err);
+      return false;
+    }
+  }
+
+  async updatePost(slug, { title, content, featuredImage, status }) {
+    try {
+      return await this.databases.updateDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug,
+        { title, content, featuredImage, status }
+      );
+    } catch (err) {
+      console.log("Update post failed in updatePost-config.js, ", err);
+      return false;
+    }
+  }
+
+  async deletePost(slug) {
+    try {
+      await this.databases.deleteDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug
+      );
+      return true;
+    } catch (err) {
+      console.log("Delete post failed in deletePost-config.js, ", err);
+      return false;
+    }
+  }
+
+  //   Storage Service
+
+  async uploadFile(file) {
+    try {
+      return await this.bucket.createFile(
+        conf.appwriteBucketId,
+        ID.unique(),
+        file
+      );
+    } catch (err) {
+      console.log("Upload file failed in uploadFile-config.js, ", err);
+      return false;
+    }
+  }
+
+  async deleteFile(fileId) {
+    try {
+      return await this.bucket.deleteFile(conf.appwriteBucketId, fileId);
+    } catch (err) {
+      console.log("Delete file failed in deleteFile-config.js, ", err);
+      return false;
+    }
+  }
+
+  async getFilePreview(fileId) {
+    try {
+      return await this.bucket.getFilePreview(conf.appwriteBucketId, fileId)
+        .href;
+    } catch (err) {
+      console.log(
+        "Getting file preview failed in getFilePreview-config.js, ",
+        err
+      );
+      return false;
+    }
+  }
+}
+
+export default service = new Service();
