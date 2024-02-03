feaa85a1a19307a6a93f92a489c14acae981309c
nishok19 <58786882+nishok19@users.noreply.github.com>
2024-02-04 01:27:29 +0530

    Created Form using React-Forms

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import Container from "../container";
+import Logo from "../Logo";
+import { Link } from "react-router-dom";
+import LogoutBtn from "./LogoutBtn";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+const Header = () => {
+  const authStatus = useSelector((state) => state.auth.status);
+  const navigate = useNavigate();
+
+  const navItems = [
+    {
+      name: "Home",
+      slug: "/",
+      active: true,
+    },
+    {
+      name: "Login",
+      slug: "/login",
+      active: !authStatus,
+    },
+    {
+      name: "Signup",
+      slug: "/signup",
+      active: !authStatus,
+    },
+    {
+      name: "All Posts",
+      slug: "/all-posts",
+      active: authStatus,
+    },
+    {
+      name: "Add Post",
+      slug: "/add-post",
+      active: authStatus,
+    },
+    {
+      name: "Logout",
+      slug: "/logout",
+      active: authStatus,
+    },
+  ];
+
+  return (
+    <header className="py-3 shadow bg-gray-500">
+      <Container>
+        <nav className="flex">
+          <div className="mr-4">
+            <Link to="/">
+              <Logo />
+            </Link>
+          </div>
+          <ul className="flex ml-auto">
+            {navItems.map((item) =>
+              item.active ? (
+                <li key={item.name}>
+                  <button onClick={() => navigate(item.slug)}>
+                    {item.name}
+                  </button>
+                </li>
+              ) : null
+            )}
+          </ul>
+        </nav>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
